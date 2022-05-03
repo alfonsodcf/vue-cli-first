82f912dcac02df88cbd67a5b533985790d7a7d34
alfonsodcf <alfonsoservizi01@gmail.com>
2022-05-03 16:57:06 +0200

    erore

---
+++ src/components/TitleTitle.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>La mia prima app con Vue CLI!</h1>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TitleTitle',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
