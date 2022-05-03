75b6f189c4b4138adc7cb978f1bfdf8eb599cb26
alfonsodcf <alfonsoservizi01@gmail.com>
2022-05-03 17:09:53 +0200

    funziona

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
-  
+  <div id="app">
+    
+    <TitleTitle msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
 import TitleTitle from './components/TitleTitle.vue'
 
 export default {
-  
+  name: 'App',
   components: {
     TitleTitle
   }
@@ -14,5 +17,7 @@ export default {
 </script>
 
 <style lang="scss">
-
+#app {
+  
+}
 </style>
